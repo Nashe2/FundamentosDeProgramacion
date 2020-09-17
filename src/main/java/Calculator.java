2a6338216b28fe818148fb24434ed12fc75af48c
Nashe <nasheog2@gmail.com>
2020-09-16 21:33:07 -0500

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    /*
+    Problema:
+
+
+    Pseudocodigo:
+
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int result = 0;
+
+        System.out.println("First input");
+        int firstInput = input.nextInt();
+        System.out.println("Second input");
+        int secondInput = input.nextInt();
+        System.out.println("Write the operation");
+        String operation = input.next();
+
+        switch (operation) {
+            case "+":
+                result = firstInput + secondInput;
+                break;
+            case "-":
+                result = firstInput - secondInput;
+                break;
+            case "*":
+                result = firstInput * secondInput;
+                break;
+            case "/":
+                result = firstInput / secondInput;
+                break;
+            default:
+                try {
+                    throw new Exception("Invalid operation");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    return;
+                }
+        }
+
+
+        System.out.println("The result is: " + result);
+    }
+}
